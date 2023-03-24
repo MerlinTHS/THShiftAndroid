ad8e50cfd7dcb0327190dc8b5b0165af2a210032
Merlin Eden (THS) <m.eden@ths-bs.de>
2023-03-24 01:30:55 +0100

    Update koin app module with event dependencies

--- app/src/main/java/de/ths/thshift/config/AppModule.kt
+++ app/src/main/java/de/ths/thshift/config/AppModule.kt
@@ -1,7 +1,31 @@
 package de.ths.thshift.config
 
+import de.ths.thshift.domain.api.event.EventRepository
+import de.ths.thshift.domain.api.event.EventViewModel
+import de.ths.thshift.domain.impl.event.KtorEventRepository
+import de.ths.thshift.domain.impl.event.SimpleEventViewModel
+import io.ktor.client.*
+import io.ktor.client.plugins.contentnegotiation.*
+import io.ktor.serialization.kotlinx.json.*
+import kotlinx.serialization.json.Json
+import org.koin.androidx.viewmodel.dsl.viewModel
 import org.koin.dsl.module
 
-val AppModule = module {
+private val homeUrl = "http://192.168.178.92:1234"
 
+private val jsonConfig = Json {
+    prettyPrint = true
+    isLenient = true
+    ignoreUnknownKeys = true
+}
+
+private val client = HttpClient {
+    install(ContentNegotiation) {
+        json(jsonConfig)
+    }
+}
+
+val AppModule = module {
+    single<EventRepository> { KtorEventRepository(homeUrl, client) }
+    viewModel<EventViewModel> { SimpleEventViewModel(get()) }
 }
